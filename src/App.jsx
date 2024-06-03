f5c1c74f65498c66bda881f8ebb9386e8aa88adb
thesimran123 <simranprajapati145@gmail.com>
2024-06-03 23:38:13 +0530

    hey

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import "react-toastify/dist/ReactTostify.css";
+//import { toast } from "react-toastify";
+
+const App = () => {
+  const user = true;
+  //const user = false;
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
+};
+
+export default App;
